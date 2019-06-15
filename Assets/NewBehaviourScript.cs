afc3d16b0f0d1e06d094aa853088e7036747a8c5
ktamulonis <kurttamulonis@gmail.com>
2019-06-15 13:59:08 -0400

    initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    
+	public int playerSpeed = 10;
+	public bool facingRight = true;
+	public int playerJumpPower = 1250;
+	public float moveX;
+    
+
+    // Update is called once per frame
+    void Update() {
+    	PlayerMove ();
+    }
+    void PlayerMove() {
+    	//CONTROLS
+    	moveX = Input.GetAxis("Horizontal");
+    	//ANIMATIONS
+    	//PLAYER DIRECTION
+    	if (moveX < 0.0f && facingRight == false) {
+    		FlipPlayer ();
+        } else if (moveX > 0.0f && facingRight == true) {
+    	    FlipPlayer ();
+        }
+    	//PHYSICS
+    	gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    	
+    }
+
+    void Jump() {
+     	//JUMPING CODE
+    }
+
+    void FlipPlayer() {
+
+    }
+}
