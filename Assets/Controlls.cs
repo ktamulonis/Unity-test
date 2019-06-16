7be7e1225da747787cc9ddb228e60a3ee049dc7d
ktamulonis <kurttamulonis@gmail.com>
2019-06-16 15:24:44 -0400

    Adds monster with animation

--- Assets/Controlls.cs
+++ Assets/Controlls.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Controlls : MonoBehaviour {
     
 	public int playerSpeed = 10;
 	public bool facingRight = true;
